08d001efb2d88844465f4c92b0140e09cf6e1297
debrah <binyanyadebby@gmail.com>
2021-03-17 16:20:51 -0700

    third commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnclose=document.queryselector('#close');
+let my tab;
+btnclose.addEventlistener('click',()=>{
+ mytab=exit.close(C:\Users\Owner\Desktop\website);
+});
+if(my tab )my tab.close();
+});
